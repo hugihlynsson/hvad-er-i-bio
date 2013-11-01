439f41814e7c2002b1a38a05e2d5e36daa7e0d8c
Hugi Hlynsson <hugihlynsson@hugis-mbp.vodafone>
2013-11-01 18:49:16 +0000

    Initial commit
    Project imported into git.

---
+++ site/index.php
@@ -0,0 +1,136 @@
+<?php
+	header('Content-Type: text/html; charset=utf-8');
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 'ON');
+
+	include 'filecache.php';
+?>
+<!DOCTYPE html>
+<html lang="is">
+	<head>
+		<meta charset="UTF-8">
+		<title>Hvað er í bíó</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="style.css">
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-20956924-1']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script');
+				ga.type = 'text/javascript';
+				ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+		<script type="text/javascript" src="//use.typekit.net/kyo1clm.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	</head>
+
+	<body>
+		<h1>Hvað er í bíó?</h1>
+
+		<div class="filters">
+			<div class="filters-wrap">
+				<div class="two-col">
+					<div class="input-box time-range">
+						<label for="from-time">Frá</label>
+						<div class="range-wrap">
+							<span class="range-mark from">00:00</span>
+							<div class="range">
+								<input id="from-time" type="range" min="0" max="24" step="0.25" name="from-time" value="0">
+								<output for="from-time" onforminput="value = from-time.valueAsNumber;">0</output>
+							</div>
+							<span class="range-mark to">24:00</span>
+						</div>
+					</div>
+					<div class="input-box time-range">
+						<label for="to-time">Til</label>
+						<div class="range-wrap">
+							<span class="range-mark from">00:00</span>
+							<div class="range">
+								<input id="to-time" type="range" min="0" max="24" step="0.25" name="to-time" value="24">
+								<output for="to-time" onforminput="value = to-time.valueAsNumber;">24</output>
+							</div>
+							<span class="range-mark to">24:00</span>
+						</div>
+					</div>
+				</div>
+				<div class="input-box rating-range">
+					<label for="rating-range">Einkunn</label>
+					<div class="range-wrap">
+						<span class="range-mark from">0</span>
+						<div class="range">
+							<input id="rating-range" type="range" min="0" max="10" step="0.1" name="rating-range" value="0">
+							<output for="rating-range" onforminput="value = rating-range.valueAsNumber;">24</output>
+						</div>
+						<span class="range-mark to">10</span>
+					</div>
+				</div>
+			</div>
+			<div class="filters-wrap place-filter">
+				<ul></ul>
+			</div>
+		</div>
+
+		<div class="movies-wrap">
+			<?php
+				$json = file_get_contents('http://apis.is/cinema');
+				$jsonDecoded = json_decode($json);
+				
+				foreach ($jsonDecoded->results as $movie) {
+					$title = $movie->title;
+					$restriction = $movie->restricted;
+					$imageUrl = $movie->image;
+					$imageType = end(explode('.', $imageUrl));
+					$imageName = urlencode($title . '.' . $imageType);
+			?>
+			<article class="movie" data-id="<?=$title?>">
+				<header>
+					<div class="rating">
+						<?=current(explode('/', $movie->imdb))?>
+						<?php if ($movie->imdb != '') { ?>
+						<span>Einkunn frá IMDB með <?=implode(array_slice(explode('10 ', $movie->imdb), 1, 1))?></span>
+						<?php } ?>
+					</div>
+					<h2><?=$movie->title?><?php if ($restriction != '') { ?>
+						<i<?php 
+							if (is_numeric(current(explode(' ', $restriction)))) 
+								echo ' class="warning"'; 
+						?>><?=$restriction?></i>
+					<?php } ?></h2>
+					<a class="mark" href="#">Merkja</a>
+				</header>
+				<a class="more" href="#">Sjá meira</a>
+				<aside class="extra">
+					<img src="<?=$imageUrl?>">	
+					<div class="padding"></div>
+					<div class="content">
+						<?php
+							foreach ($movie->showtimes as $showtime) {
+								$theater = $showtime->theater;
+						?>
+						<div class="showplace" data-place="<?=$theater?>">
+							<h3><?=$theater?>:</h3>
+							<ul>
+								<?php foreach ($showtime->schedule as $time) { ?>
+								<li data-time="<?=current(explode(' ', $time))?>"><?=$time?></li>
+								<?php } ?>
+							</ul>
+						</div>
+						<?php } ?>
+					</div>
+				</aside>
+			</article>
+			<?php } ?>
+		</div>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script src="main.js"></script>
+	</body>
+</html>
+
+<?php
+	// writeCache($cacheFile, ob_get_contents());
+?>
---
+++ site/main.js
@@ -0,0 +1,350 @@
+
+//
+// ======================= HELPING FUNCTIONS ===========================
+// 
+
+
+// Change number with point (20.5) to human readable string (20:30):
+var numToTime = function (number) {
+	var parts = number.split('.');
+	if (parts.length === 1 || parseInt(parts[1]) === 0) return number + ':00';
+	minutes = Math.round(parseFloat('0.' + parts[1])*60).toString();
+	if (minutes.length === 1) minutes = '0' + minutes;
+	return parts[0] + ':' + minutes;
+};
+
+
+// Change human readable (20:30) time to number with point (20.5) as string:
+var timeToNum = function (time) {
+	var parts = time.split(':');
+	return parts[0] + (parseInt(parts[1])/60 + '').substring(1);
+};
+
+
+var closeArticleExtra = function (article) {
+	article.removeClass('open');
+	article.find('.more').text('Sjá meira');
+	article.find('.extra').slideUp(300);
+};
+
+var openArticleExtra = function (article) {
+	article.addClass('open');
+	article.find('.more').text('Sjá minna');
+	article.find('.extra').slideDown(300);
+	$('html,body').clearQueue();
+	$('html,body').animate({ scrollTop: $(article).offset().top}, 400);
+};
+
+
+
+//
+// ========================= MAIN FUNCTIONS ============================
+// 
+
+
+// ======== BASE STUFF ========
+
+// An object containing all info for the filtering:
+var movies = {};
+var places = {};
+
+// Mark movies with highest and lowest showtimes:
+var populateMoviesObject = function () {
+
+	movies.lowestShowtime = 24;
+	movies.highestShowtime = 0;
+	movies.titles = {};
+	// Populate the 'movies' with movies:
+	$('.movie').each(function () {
+		var movieElm = $(this);
+		var id = movieElm.data('id');
+		movies.titles[id] = {};
+		var movie = movies.titles[id];
+		movie.isFiltered = false;
+		var rating = parseFloat(movieElm.find('.rating').text().split('Ein')[0]);
+		movie.rating = (rating != '') ? rating : '10';
+		movie.places = {};
+
+		// For each movie object, add the showplaces:
+		movieElm.find('.showplace').each(function () {
+			var placeElm = $(this);
+			var placeStr = placeElm.data('place');
+			if (!(placeStr in places)) places[placeStr] = true;
+			movie.places[placeStr] = {};
+			var place = movie.places[placeStr];
+
+			// For each showplace array, add the time:
+			placeElm.find('li').each(function () {
+				var time = timeToNum($(this).data('time'));
+				place[time] = 'visible';
+
+				// Recalculate lowest and highest showtime:
+				if (time < movies.lowestShowtime) movies.lowestShowtime = time;
+				if (time > movies.highestShowtime) movies.highestShowtime = time;
+			});
+		});
+	});
+};
+
+
+// ======== FILTER PREFERENCES ========
+
+// Mark movies with highest and lowest showtimes:
+var filterMoviesByTime = function (lowFilter, highFilter) {
+	for (var id in movies.titles) {
+		var hasTime = false;
+
+		// Check if movie has any time that passes the filter:
+		var places = movies.titles[id].places;
+		for (var placeKey in places) {
+			var place = places[placeKey];
+			for (var time in place) {
+				if (lowFilter <= time && time <= highFilter) {
+					hasTime = true;
+					place[time] = 'visible';
+				}
+				else { place[time] = 'filtered'; }
+			}
+		}
+
+		if (!hasTime) movies.titles[id].isFiltered = true;
+	}
+};
+
+var filterMoviesByRating = function (minRating) {
+	for (var id in movies.titles) {
+		if (movies.titles[id].rating < minRating) {
+			movies.titles[id].isFiltered = true;
+		}
+	}
+}
+
+var filterMoviesByPlace = function (placesAllowed) {
+	if ($.isEmptyObject(placesAllowed)) return;
+	for (var id in movies.titles) {
+		var hasPlace = false;
+		
+		// Check if movie has any place that passes the filter:
+		var places = movies.titles[id].places;
+		for (var place in places) {
+			if (place in placesAllowed) {
+				hasPlace = true;
+				break;
+			}
+		}
+
+		if (!hasPlace) movies.titles[id].isFiltered = true;
+	}
+}
+
+var filterMovies = function () {
+	// Start unfiltering every movie:
+	for (var id in movies.titles) {
+		movies.titles[id].isFiltered = false;
+	}
+
+	// Now apply the filters:
+	filterMoviesByTime($('#from-time').val(), $('#to-time').val());
+	filterMoviesByRating($('#rating-range').val());
+	filterMoviesByPlace(getToggledPlaces());
+
+	// Hide and show movies based on filtering:
+	$('.movie').each(function () {
+		var movieElm = $(this);
+		var id = movieElm.data('id');
+		if (movies.titles[id].isFiltered) {
+			movieElm.slideUp();
+			closeArticleExtra(movieElm);
+		}
+		else {
+
+			movieElm.find('.showplace').each(function () {
+				var place = $(this).data('place');
+				$(this).find('li').each(function () {
+					var time = timeToNum($(this).data('time'));
+					if (movies.titles[id].places[place][time] === 'visible') {
+						$(this).removeClass('filtered');
+					}
+					else {
+						$(this).addClass('filtered');
+					}
+				});
+			}); 
+
+			movieElm.slideDown();
+		}
+	});
+
+}
+
+
+var limitTimeRange = function() {
+	$('#to-time, #from-time').attr('min', movies.lowestShowtime);
+	$('#to-time, #from-time').attr('max', movies.highestShowtime);
+	$('.time-range .from').text(numToTime(movies.lowestShowtime));
+	$('.time-range .to').text(numToTime(movies.highestShowtime));
+}
+
+var initPlaceFilter = function () {
+	var placeFilterList = $('.place-filter ul');
+	for (place in places) {
+		placeFilterList.append('<li>' +  place + '</li>')
+	}
+
+	$('.place-filter li').on('click', function () {
+		$(this).toggleClass('toggled', !$(this).is('.toggled'));
+		filterMovies();
+	});
+};
+
+var updateRangemarkPosition = function (range) {
+	var el, newPoint, newPlace, offset;
+	// Compersate for thumb width:
+	width = range.width()-20;
+	newPoint = (range.val()-range.attr("min")) / (range.attr("max")-range.attr("min"));
+	// Compersate for thumb width and slider padding:
+	offset = 10;
+	// Prevent mark from going beyond left or right (unsupported browsers):
+	if (newPoint < 0) { newPlace = 0; }
+	else if (newPoint > 1) { newPlace = width; }
+	else { newPlace = width * newPoint + offset; offset -= newPoint; }
+
+	range.next('output').css('left', newPlace)
+}
+
+var updateTimeRangeMarks = function () {
+
+	// Keep from_time lower than to_time and the other way around:
+	var fromTime = $('#from-time').val();
+	var toTime = $('#to-time').val();
+	if (fromTime >= toTime) { $('#to-time').val(parseFloat(fromTime)); }
+	if (toTime <= fromTime) { $('#from-time').val(parseFloat(toTime)); }
+
+	// Update position and value of rangemarks:
+	$('#to-time, #from-time').each(function () {
+		var range = $(this);
+		updateRangemarkPosition(range);
+		range.next('output').text(numToTime(range.val()));
+	});
+};
+
+var activateLocalStorageMarks = function () {
+	// Load marked things from localstorage:
+	$('.movie').each(function() {
+		var movie = $(this);
+		if (window.localStorage.getItem(movie.data('id') + '.marked')) {
+			movie.addClass('marked');
+		}
+		else if (window.localStorage.getItem(movie.data('id') + '.seen')) {
+			movie.addClass('seen');
+		}
+		movie.find('.showplace li').each(function () {
+			var place = $(this).closest('.showplace').find('h3').text();
+			if (window.localStorage.getItem(
+				(movie).data('id') + place + $(this).text() + '.marked'
+			)) {
+				$(this).addClass('marked');
+			}
+		});
+	});
+
+	// Toggle marked on article and localstorage:
+	$('.mark').on('click', function (e) {
+
+		e.preventDefault();
+
+		var article = $(this).closest('article');
+		if (article.is('.marked')) {
+			article.removeClass('marked');
+			article.addClass('seen');
+			window.localStorage.removeItem(article.data('id') + '.marked');
+			window.localStorage.setItem(article.data('id') + '.seen', true);
+		}
+		else if (article.is('.seen')) {
+			article.removeClass('seen');
+			window.localStorage.removeItem(article.data('id') + '.seen');
+		}
+		else {
+			article.addClass('marked');
+			window.localStorage.setItem(article.data('id') + '.marked', true);
+		}
+	});
+
+	// Toggle marked on showtime and localstorage:
+	$('.showplace li').on('click', function () {
+		var time = $(this);
+		var movie = time.closest('.movie');
+		var place = time.closest('.showplace').find('h3').text();
+
+		if (time.is('.marked')) {
+			time.removeClass('marked');
+			window.localStorage.removeItem(
+				(movie).data('id') + place + $(this).text() + '.marked'
+			);
+		}
+		else {
+			time.addClass('marked');
+			window.localStorage.setItem(
+				(movie).data('id') + place + $(this).text() + '.marked',
+				'true'
+			);
+		}
+	});
+};
+
+// Show the aside and change link state:
+var activateMoreToggle = function () {
+	$('.more').on('click', function (e) {
+		e.preventDefault();
+		var article = $(this).closest('article');
+
+		if (article.is('.open')) closeArticleExtra(article);
+		else openArticleExtra(article);
+	});
+};
+
+var activateRangeSlider = function () {
+	// On time range change:
+	$('#to-time, #from-time').on('change', function() {
+		filterMovies();
+		updateTimeRangeMarks();	
+	});
+
+	// On rating range change:
+	$('#rating-range').on('change', function () {
+		var range = $(this);
+		filterMovies();
+		updateRangemarkPosition(range);
+		range.next('output').text(range.val());
+	}).trigger('change');
+
+	$(window).on('resize', function () {
+		$('input[type="range"]').each(function () {
+			updateRangemarkPosition($(this));
+		});
+	});
+};
+
+var getToggledPlaces = function () {
+	var places = {};
+	$('.place-filter li').each(function () {
+		if ($(this).is('.toggled')) {
+			places[$(this).text()] = true;
+		}
+	});
+	return places;
+};
+
+//
+// ========================= INITIALIZATION ============================
+// 
+
+// Has to happen first:
+populateMoviesObject();
+
+initPlaceFilter();
+limitTimeRange();
+updateTimeRangeMarks();
+activateLocalStorageMarks();
+activateMoreToggle();
+activateRangeSlider();
