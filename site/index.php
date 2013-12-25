ec7446f73faa0a86d3ef97f825af61f83141a24b
Hugi Hlynsson <hugihlynsson@gmail.com>
2013-12-25 16:08:51 +0000

    Add tracking code

--- site/index.php
+++ site/index.php
@@ -23,17 +23,11 @@
 		<link rel="stylesheet" href="style.css">
 
 		<script>
-			(function(i,s,o,g,r,a,m){
-				i['GoogleAnalyticsObject'] = r;
-				i[r] = i[r] || function() {
-					(i[r].q = i[r].q || []).push(arguments)
-				}, i[r].l = 1 * new Date();
-				a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-				a.async = 1;
-				a.src = g;
-				m.parentNode.insertBefore(a,m)
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-20956924-1', 'hvaderibio.is');
+			ga('create', 'UA-20956924-4', 'hvaderibio.is');
 			ga('send', 'pageview');
 		</script>
 
