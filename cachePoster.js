a5cba563eda56bee87e418824807f2bb4df272db
Hugi Hlynsson <hugihlynsson@gmail.com>
2014-11-26 19:23:19 +0000

    Fix indentation

--- cachePoster.js
+++ cachePoster.js
@@ -6,14 +6,14 @@ var request = require('request');
 
 // Returns a relative url to the cached
 var cachePoster = function(url, title) {
-  var fileName = sanitize(title) + '.' + url.split('.').pop();
+    var fileName = sanitize(title) + '.' + url.split('.').pop();
 
-  // Todo: Invalidate the cache if too old
-  if (!fs.existsSync('./public/posters/' + fileName)) {
-    request(url).pipe(fs.createWriteStream('./public/posters/' + fileName));
-  }
+    // Todo: Invalidate the cache if too old
+    if (!fs.existsSync('./public/posters/' + fileName)) {
+        request(url).pipe(fs.createWriteStream('./public/posters/' + fileName));
+    }
 
-  return 'posters/' + encodeURIComponent(fileName).replace(/[!'()]/g, '').replace(/\*/g, '%2A');
+    return 'posters/' + encodeURIComponent(fileName).replace(/[!'()]/g, '').replace(/\*/g, '%2A');
 };
 
 module.exports = cachePoster;
